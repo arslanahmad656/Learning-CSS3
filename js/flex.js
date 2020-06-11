3951c48c1ffa8498c8a4a7ea76d59038e10b0932
Arslan Ahmad 656 <arslan_mgl6445@live.com>
2020-06-11 16:46:29 +0500

    flex file added

---
+++ js/flex.js
@@ -0,0 +1,19 @@
+function setFlexDirection(value) {
+    document.getElementById('parent').style.flexDirection = value;
+}
+
+function setFlexWrap(value) {
+    document.getElementById('parent').style.flexWrap = value;
+}
+
+function setJustifyContent(value) {
+    document.getElementById('parent').style.justifyContent = value;
+}
+
+function setAlignContent(value) {
+    document.getElementById('parent').style.alignContent = value;
+}
+
+function setAlignItems(value) {
+    document.getElementById('parent').style.alignItems = value;
+}
